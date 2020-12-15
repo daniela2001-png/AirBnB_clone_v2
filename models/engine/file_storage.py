96efe1444c9a30fd825b2d5fb52eb20db96c16d0
daniela2001-png <dmbmora@outlook.com>
2020-12-15 16:49:03 -0500

    [new] adding method delete() to filestorage()

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,8 +55,4 @@ class FileStorage:
         """
         if obj:
             key = "{}.{}".format(type(obj).__name__, obj.id)
-            if key in self.__objects:
-                del self.__objects[key]
-                self.save()
-        else:
-            return
+            del self.__objects[key]
