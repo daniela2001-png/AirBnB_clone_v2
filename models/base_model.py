a777ab8e9c7edaf0d73a3b90c5b90c75d8b8b134
Alison Quintero <alisondanellaquintero@hotmail.com>
2020-12-15 18:10:28 -0500

    Delete function

--- models/base_model.py
+++ models/base_model.py
@@ -7,20 +7,30 @@ from datetime import datetime
 class BaseModel:
     """A base class for all hbnb models"""
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
-        if not kwargs:
+        """
+        Arguments
+          - *args: is a Tuple that contains all arguments (won’t be used).
+          - **kwargs: is a dictionary that contains all arguments by key/value.
+        Functions/methods:
+          - strptime(): creates a datetime object from the given instance.
+          - setattr(): sets the value of the attribute of an object.
+        """
+
+        if kwargs:
+            for k, v in kwargs.items():
+                if k == '__class__':
+                    pass
+                elif k == 'created_at' or k == 'updated_at':
+                    setattr(self, k, datetime.
+                            strptime(v, "%Y-%m-%dT%H:%M:%S.%f"))
+                else:
+                    setattr(self, k, v)
+        else:
             from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             storage.new(self)
-        else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
 
     def __str__(self):
         """Returns a string representation of the instance"""
