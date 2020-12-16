1482eb843179f10a248f6882791349034503d327
Camilo <trresqwe@gmail.com>
2020-12-16 12:17:28 -0500

    Update console

--- console.py
+++ console.py
@@ -77,7 +77,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is '}'\
+                    if pline[0] == '{' and pline[-1] == '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -92,7 +92,7 @@ class HBNBCommand(cmd.Cmd):
 
     def postcmd(self, stop, line):
         """Prints if isatty is false"""
-        if not sys.__stdin__.isatty():
+        if not sys._stdin_.isatty():
             print('(hbnb) ', end='')
         return stop
 
@@ -259,7 +259,7 @@ class HBNBCommand(cmd.Cmd):
             else:
                 objects = storage.all(args)
         except NameError:
-            print("** class doesn't exist **")
+            print("* class doesn't exist *")
             return
         for key, val in objects.items():
             if len(args) != 0:
@@ -327,7 +327,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -335,10 +335,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
@@ -366,7 +366,7 @@ class HBNBCommand(cmd.Cmd):
                     att_val = HBNBCommand.types[att_name](att_val)
 
                 # update dictionary with name, value pair
-                new_dict._dict_.update({att_name: att_val})
+                new_dict.dict.update({att_name: att_val})
 
         new_dict.save()  # save updates to file
 
