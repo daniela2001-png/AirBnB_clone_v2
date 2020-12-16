248ec6bafb85e77fa27938e6ef9f5598ca26c099
daniela2001-png <dmbmora@outlook.com>
2020-12-15 19:42:53 -0500

    [new] addin changes in base_model

--- models/base_model.py
+++ models/base_model.py
@@ -55,13 +55,11 @@ class BaseModel:
 
     def to_dict(self):
         """Convert instance into dict format"""
-        dictionary = {}
-        dictionary.update(self._dict_)
-        dictionary.update({'_class_':
-                           (str(type(self)).split('.')[-1]).split('\'')[0]})
+        dictionary = dict(self.__dict__)
+        dictionary["__class__"] = str(type(self).__name__)
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
-        if "_sa_instance_state" is in dictionary.keys():
+        if "_sa_instance_state" in dictionary.keys():
             del dictionary["_sa_instance_state"]
         return dictionary
 
