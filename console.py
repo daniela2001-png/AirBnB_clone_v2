fb3df059c12f38309182a27f0d517f9807434345
Camilo <trresqwe@gmail.com>
2020-12-16 14:44:14 -0500

    Update console

--- console.py
+++ console.py
@@ -218,25 +218,21 @@ class HBNBCommand(cmd.Cmd):
 
     def do_all(self, args):
         """ Shows all objects, or all objects of a class"""
-        obj_list = []
-        storage = models.storage
-        storage.reload()
-        try:
-            if len(args) != 0:
-                eval(args)
-            if len(args) == 0:
-                objects = storage.all()
-            else:
-                objects = storage.all(args)
-        except NameError:
-            print("** class doesn't exist **")
-            return
-        for key, val in objects.items():
-            if len(args) != 0:
-                if type(val) is eval(args):
-                    obj_list.append(val)
-            else:
-                obj_list.append(val)
+        print_list = []
+
+        if args:
+            args = args.split(' ')[0]  # remove possible trailing args
+            if args not in HBNBCommand.classes:
+                print("** class doesn't exist **")
+                return
+            for k, v in storage._FileStorage__objects.items():
+                if k.split('.')[0] == args:
+                    print_list.append(str(v))
+        else:
+            for k, v in storage._FileStorage__objects.items():
+                print_list.append(str(v))
+
+        print(print_list)
 
     def help_all(self):
         """ Help information for the all command """
