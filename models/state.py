86b001dadea32e855e58d099ee9687dc9919e3d5
Camilo <trresqwe@gmail.com>
2020-12-16 15:04:33 -0500

    Update state

--- models/state.py
+++ models/state.py
@@ -2,9 +2,13 @@
 """ States Module for HBNB project"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state",
+                          cascade="all, delete, delete-orphan",
+                          primaryjoin="City.state.id==State.id")
