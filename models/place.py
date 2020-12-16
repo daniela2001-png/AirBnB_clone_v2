d7ab1d365cc6255a8a3fea623549ff74c6f78702
Alison Quintero <alisondanellaquintero@hotmail.com>
2020-12-16 16:29:13 -0500

    DBStorage-Place

--- models/place.py
+++ models/place.py
@@ -18,18 +18,19 @@ class Place(BaseModel, Base):
     Inherits from SQLAlchemy Base and links to the MySQL table places.
     """
     __tablename__ = "places"
+
     city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
     user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
     name = Column(String(128), nullable=False)
-    description = Column(String(1024))
-    number_rooms = Column(Integer, default=0)
-    number_bathrooms = Column(Integer, default=0)
-    max_guest = Column(Integer, default=0)
-    price_by_night = Column(Integer, default=0)
-    latitude = Column(Float)
-    longitude = Column(Float)
-    reviews = relationship("Review", backref="place", cascade="delete")
+    description = Column(String(1024), nullable=True)
+    number_rooms = Column(Integer, default=0, nullable=False)
+    number_bathrooms = Column(Integer, default=0, nullable=False)
+    max_guest = Column(Integer, default=0, nullable=False)
+    price_by_night = Column(Integer, default=0, nullable=False)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
     amenity_ids = []
+    reviews = relationship("Review", backref="place", cascade="delete")
 
     if getenv("HBNB_TYPE_STORAGE", None) != "db":
         @property
