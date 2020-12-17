ed91abb5aae6813738ca6890805d0f6cf546ebef
daniela2001-png <dmbmora@outlook.com>
2020-12-17 14:07:11 -0500

    [change] fixed method getter cities

--- models/state.py
+++ models/state.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
+import os
 
 
 class State(BaseModel, Base):
@@ -10,3 +11,13 @@ class State(BaseModel, Base):
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state", cascade="delete")
+
+    if (os.getenv("HBNB_TYPE_STORAGE") != "db"):
+        @property
+        def cities(self):
+            """Get a list of all related City objects."""
+            city = []
+            for obj in list(models.storage.all(City).values()):
+                if city.state_id == self.id:
+                    city.append(obj)
+            return city
