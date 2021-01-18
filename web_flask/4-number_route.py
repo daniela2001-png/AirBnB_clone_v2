36ba11e4ffdf175222a9149582ac09364437a96f
daniela2001-png <dmbmora@outlook.com>
2021-01-18 16:52:04 -0500

    fixed docs

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -10,16 +10,26 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"], strict_slashes=False)
 def index():
+    """
+    main index page
+    """
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", methods=["GET"], strict_slashes=False)
 def hbnb():
+    """
+    return string
+    """
     return "HBNB"
 
 
 @app.route("/c/<string:text>", methods=["GET"], strict_slashes=False)
 def cisfun(text):
+    """
+    using props
+    string : true
+    """
     print("C {}".format(text.replace("_", " ")))
 
 
@@ -27,11 +37,17 @@ def cisfun(text):
            strict_slashes=False)
 @app.route("/python/<string:text>", methods=["GET"], strict_slashes=False)
 def python(text):
+    """
+    props and default values
+    """
     return ("Python is {}".format(text.replace("_", " ")))
 
 
 @app.route("/number/<int:n>", methods=["GET"], strict_slashes=False)
 def number(n):
+    """
+    is a number ? or no
+    """
     return "{} is a number".format(n)
 
 
