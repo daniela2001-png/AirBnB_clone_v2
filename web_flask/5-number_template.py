e26a1fd5d29435073148f180600bf4ddc96ec620
daniela2001-png <dmbmora@outlook.com>
2021-01-18 16:54:24 -0500

    fixed docs

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -10,16 +10,25 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"], strict_slashes=False)
 def index():
+    """
+    a simple string to return
+    """
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", methods=["GET"], strict_slashes=False)
 def hbnb():
+    """
+    a simple string to return
+    """
     return "HBNB"
 
 
 @app.route("/c/<string:text>", methods=["GET"], strict_slashes=False)
 def cisfun(text):
+    """
+    string without defautl values
+    """
     print("C {}".format(text.replace("_", " ")))
 
 
@@ -27,16 +36,26 @@ def cisfun(text):
            strict_slashes=False)
 @app.route("/python/<string:text>", methods=["GET"], strict_slashes=False)
 def python(text):
+    """
+    python is (text)
+    default value: python is cool
+    """
     return ("Python is {}".format(text.replace("_", " ")))
 
 
 @app.route("/number/<int:n>", methods=["GET"], strict_slashes=False)
 def number(n):
+    """
+    is a  number or no ?
+    """
     return "{} is a number".format(n)
 
 
 @app.route("/number_template/<int:n>", methods=["GET"], strict_slashes=False)
 def number_template(n):
+    """
+    using jinja templates
+    """
     return render_template("5-number.html", n=n)
 
 
