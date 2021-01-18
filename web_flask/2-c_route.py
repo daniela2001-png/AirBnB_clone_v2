e6b8399cbec05a3b88176b47cea80eaf35506bdf
daniela2001-png <dmbmora@outlook.com>
2021-01-18 14:41:42 -0500

    [Add] adding another route with  a prop

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Comenzando con Flask!
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"], strict_slashes=False)
+def index():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", methods=["GET"], strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<string:text>", methods=["GET"], strict_slashes=False)
+def cisfun(text):
+    print("C {}".format(text.replace("_", " ")))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
